01137d4e728af9070daa92092f73c1a0381503d6
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2020-11-11 11:44:49 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	import {fade, slide} from "svelte/transition";
+	import Button from "./modules/Bouton.svelte";
+	let total = 0;
+
+	function ajouter(nombre) {
+		total += nombre;
+	}
+
+	$: carré = total * total;
+</script>
+
+<style>
+</style>
+
+<main transition:fade={{duration: 1000}}>
+	<h1>{total}</h1>
+	<p>Le carré est {carré}</p>
+	{#if carré > 2500}
+		<div class="turfu" transition:slide>Futur salaire ? ;) 🍾</div>
+	{/if}
+
+	<Button
+		on:click={() => {
+			ajouter(1);
+		}}>
+		AJOUTER 1
+	</Button>
+
+	<Button
+		on:click={() => {
+			ajouter(10);
+		}}>
+		AJOUTER 10
+	</Button>
+</main>
